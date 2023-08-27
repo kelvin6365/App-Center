a97efb91115697322fb19980e55fcdc4a3ab4268
kelvin_kwong <kelvin6365@gmail.com>
2023-08-27 22:20:58 +0800

    update permission

---
+++ apps/backend/src/app/database/migrations/1693145873440-permission.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Permission1693145873440 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+    create table if not exists public.permissions
+    (
+        id         uuid      default uuid_generate_v4() not null
+            constraint "PK_920331560282b8bd21bb02290df"
+                primary key,
+        created_at timestamp default now()              not null,
+        updated_at timestamp default now()              not null,
+        deleted_at timestamp,
+        version    integer                              not null,
+        created_by uuid,
+        updated_by uuid,
+        type       varchar                              not null
+    );`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`drop table if exists public.permissions;`);
+  }
+}
