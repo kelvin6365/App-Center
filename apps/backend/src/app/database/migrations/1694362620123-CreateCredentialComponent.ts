2269ddda16ff823a582ba7c1c857cb295e182685
kelvin_kwong <kelvin6365@gmail.com>
2023-09-11 00:52:37 +0800

    icon missing

--- apps/backend/src/app/database/migrations/1694362620123-CreateCredentialComponent.ts
+++ apps/backend/src/app/database/migrations/1694362620123-CreateCredentialComponent.ts
@@ -7,18 +7,20 @@ export class CreateCredentialComponent1694362620123
     await queryRunner.query(`
     create table if not exists public.credential_components
     (
-        id         uuid      default uuid_generate_v4() not null
+        id          uuid      default uuid_generate_v4() not null
             constraint "PK_d3a2c6fe030171d8c785df64ef8"
                 primary key,
-        created_at timestamp default now()              not null,
-        updated_at timestamp default now()              not null,
-        deleted_at timestamp,
-        version    integer                              not null,
-        created_by uuid,
-        updated_by uuid,
-        label      varchar                              not null,
-        name       varchar                              not null,
-        inputs     jsonb                                not null
+        created_at  timestamp default now()              not null,
+        updated_at  timestamp default now()              not null,
+        deleted_at  timestamp,
+        version     integer                              not null,
+        created_by  uuid,
+        updated_by  uuid,
+        label       varchar                              not null,
+        icon        varchar                              not null,
+        name        varchar                              not null,
+        description varchar,
+        inputs      jsonb                                not null
     );`);
   }
 
