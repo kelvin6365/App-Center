f7d5d058760196701944991fa02e9c0e1f54e56a
kelvin_kwong <kelvin6365@gmail.com>
2023-09-11 00:26:25 +0800

    migration ready

--- apps/backend/src/app/database/database.module.ts
+++ apps/backend/src/app/database/database.module.ts
@@ -20,6 +20,8 @@ import { Permission1693145873440 } from './migrations/1693145873440-permission';
 import { PermissionInsert1693145971058 } from './migrations/1693145971058-permission-insert';
 import { Credential } from '../modules/credential/entities/credential.entity';
 import { CredentialComponent } from '../modules/credential/entities/credential.component.entity';
+import { CreateCredentialComponent1694362620123 } from './migrations/1694362620123-CreateCredentialComponent';
+import { InsertCredentialComponent1694362717025 } from './migrations/1694362717025-InsertCredentialComponent';
 
 @Module({
   imports: [
@@ -54,6 +56,8 @@ import { CredentialComponent } from '../modules/credential/entities/credential.c
             role1693123446948,
             Permission1693145873440,
             PermissionInsert1693145971058,
+            CreateCredentialComponent1694362620123,
+            InsertCredentialComponent1694362717025,
           ],
           migrationsRun: configService.get('db.migrationsRun'),
           logging: configService.get('db.logging'),
